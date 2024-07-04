729de2a4c15a965a82a6a0e2cb8918b058480c18
sangyeopchae <sy.chae@teamsparta.co>
2024-07-05 00:04:40 +0900

    feat: class-validator, transformer 의존성 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ApiExceptionFilter } from './common/filter/api-exception.filter';
@@ -5,6 +6,13 @@ import { ApiExceptionFilter } from './common/filter/api-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ApiExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
